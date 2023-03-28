c334384bc86eb302324a6d8d620441596bbdafee
IGiraudo <127528724+IGiraudo@users.noreply.github.com>
2023-03-28 15:33:20 -0700

    Add files via upload

---
+++ practicas/tp-complejidad/code/complejidad.py
@@ -0,0 +1,106 @@
+from algo1 import *
+from mylinkedlist import *
+from random import randint
+
+# Ejercicio 4:
+
+'''Tomamos el valor del medio, vemos los lugares que tiene antes que él, la mitad de esta cantidad tienen que tener valores menores que el valor del medio.
+La cantidad de números que tengamos antes de nuestro middle_value va a ser igual al número de su posición.
+Guardamos las posiciones de los elementos que tienen un valor mayor a middle_value y se encuentran posicionados antes que él.
+Luego si no tenemos los valores menores necesarios, realizamos un intercambio de un valor menor que esté posicionado a la derecha con uno guardado en la lista antes mencionada'''
+
+
+def middle_value_sort(array):
+	middle_index = int(len(array)/2)
+	print("middle value:", array[middle_index])
+	minor_values = 0
+	major_values_positions = LinkedList()
+	minor_values_positions = LinkedList()
+
+	for i in range(0, middle_index):
+		if(array[i] < array[middle_index]):
+			minor_values += 1
+			add(minor_values_positions, i)
+		elif(array[i] > array[middle_index]):
+			add(major_values_positions, i)
+
+	# están los valores menores necesarios a la izquierda
+	if(minor_values == int(middle_index/2)): 
+		return array
+
+	# faltan valores menores a la izquierda
+	if(minor_values < int(middle_index/2)): 
+		for i in range(middle_index+1, len(array)):
+			if(array[i] < array[middle_index]):
+				if(major_values_positions.head == None):
+					return array
+				
+				swapPositions(array, i, major_values_positions.head.value)
+				deletePosition(major_values_positions, 0)
+				minor_values += 1
+			if(i == len(array)-1):
+				return array
+
+		if(minor_values == int(middle_index/2)):
+			return array
+
+	# hay valores menores de más a la izquierda
+	if(minor_values > int(middle_index/2)): 
+		for i in range(middle_index+1, len(array)):
+			if(array[i] >= array[middle_index]):
+				swapPositions(array, i, minor_values_positions.head.value)
+				deletePosition(minor_values_positions, 0)
+				minor_values -= 1
+			if(minor_values == int(middle_index/2)):
+				return array
+			if(i == len(array)-1):
+				return array
+			
+
+def swapPositions(array, pos1, pos2):
+	temp = array[pos1]
+	array[pos1] = array[pos2]
+	array[pos2] = temp
+	#array[pos1], array[pos2] = array[pos2], array[pos1]
+	return array
+
+newArray = Array(9, 0)
+for i in range(0, len(newArray)):
+	newArray[i] = randint(0, 10)
+
+print(newArray)
+print(middle_value_sort(newArray))
+
+
+# Ejercicio 5: 
+
+'''Podemos formar una nueva lista sin los números repetidos en la lista y sin los números que son mayores o iguales a n, ya que no van a poder formar sumas = n
+Con mergesort y binary search se puede bajar la complejidad a O(nlogn)'''
+# O(n²)
+def contiene_suma(array,n):
+	for i in range(0, len(array)):
+		for j in range(i+1, len(array)):
+			if(i != j):
+				if(array[i]+array[j] == n):
+					return True
+	return False
+
+print(contiene_suma(newArray, 10))
+
+#Ejercicio 5 
+
+def ContieneSuma(A, n):
+  current = A.head
+  aux = A.head
+  if aux.value+current.nextNode.value==n:
+    return true
+  else:
+    while current != None and aux != None: #o(n) peor caso-#O(C)
+      if current.nextnode != None:
+        if aux.value + current.nextnode.value == n:
+          return True
+        current = current.nextNode
+      else:
+        aux = aux.nextNode
+        current = aux
+    return
---
+++ practicas/tp-complejidad/code/mylinkedlist.py
@@ -0,0 +1,289 @@
+class LinkedList:
+	head = None
+
+class Node:
+	value = None
+	nextNode = None
+
+# Agrega un elemento al comienzo de lista
+# Salida: ​ No hay salida definida
+def add(L,element):
+	current = L.head
+	newNode = Node()
+	newNode.value = element
+
+	if current == None:
+		L.head = newNode
+	else:
+		newNode.nextNode = current
+		L.head = newNode
+	return
+
+# Agrega un nodo al comienzo de lista
+# Salida: ​ No hay salida definida
+def addNode(L,node):
+	current = L.head
+
+	if current == None:
+		L.head = node
+	else:
+		node.nextNode = current
+		L.head = node
+	return
+
+# Descripción: ​Busca un elemento de la lista
+# Salida​: Devuelve la posición donde se encuentra la primera instancia del elemento, Devuelve None​ si el elemento no se encuentra.
+def search(L,element):
+	current = L.head
+	currentPos = 0
+
+	while current != None:
+		if current.value == element:
+			return currentPos
+
+		current = current.nextNode
+		currentPos += 1
+	return
+
+# Descripción: ​Busca un elemento de la lista
+# Salida​: Devuelve la posición donde se encuentra la primera instancia del elemento, Devuelve None​ si el elemento no se encuentra.
+def searchNode(L,node):
+	current = L.head
+	currentPos = 0
+
+	while current != None:
+		if current == node:
+			return currentPos
+
+		current = current.nextNode
+		currentPos += 1
+	return  
+
+# Descripción:​ Inserta un elemento en una posición determinada de la lista que representa el TAD​ ​secuencia​.
+# Salida​: Si pudo insertar con éxito devuelve la posición donde se inserta el elemento. En caso contrario devuelve ​None​. Devuelve None si la posición a insertar es mayor que el número de elementos en la lista.
+def insert(L,element,position):
+	current = L.head
+	currentPos = 0
+
+	if position == 0:
+		add(L,element)
+		return position
+
+	elif position > 0:
+		if current == None: return
+		newNode = Node()
+		newNode.value = element
+
+		while current.nextNode != None:
+			if currentPos+1 != position:
+				current = current.nextNode
+				currentPos += 1
+			else:
+				newNode.nextNode = current.nextNode
+				current.nextNode = newNode
+				return position
+		current.nextNode = newNode
+		return currentPos+1
+	return
+
+# Descripción:​ Inserta un nodo en una posición determinada de la lista que representa el TAD​ ​secuencia​.
+# Salida​: Si pudo insertar con éxito devuelve la posición donde se inserta el elemento. En caso contrario devuelve ​None​. Devuelve None si la posición a insertar es mayor que el número de elementos en la lista.
+def insertNode(L,node,position):
+	current = L.head
+	currentPos = 0
+
+	if position == 0:
+		addNode(L,node)
+		return position
+
+	elif position > 0:
+		if current == None: return
+
+		while current.nextNode != None:
+			if currentPos+1 != position:
+				current = current.nextNode
+				currentPos += 1
+			else:
+				node.nextNode = current.nextNode
+				current.nextNode = node
+				return position
+		current.nextNode = node
+		return currentPos+1
+	return
+
+# Elimina un elemento de la lista
+# Poscondición:​ Se debe desvincular el ​ Node​ a eliminar.
+# Salida​ : Devuelve la posición donde se encuentra el elemento a eliminar. Devuelve ​ None​ si el elemento a eliminar no se encuentra
+def delete(L,element):
+	current = L.head
+	position = search(L, element)
+	if position == None: return
+	if position == 0: 
+		L.head = L.head.nextNode
+		return position
+
+	for i in range(0, position-1):
+		current = current.nextNode
+	current.nextNode = current.nextNode.nextNode
+	return position
+
+# Elimina un nodo de la lista
+# Poscondición:​ Se debe desvincular el ​Node​ a eliminar.
+# Salida​ : Devuelve la posición donde se encuentra el elemento a eliminar. Devuelve ​ None​ si el elemento a eliminar no se encuentra
+def deleteNode(L,node):
+	current = L.head
+	position = searchNode(L, node)
+	if position == None: return
+	if position == 0: 
+		L.head = L.head.nextNode
+		return position
+
+	for i in range(0, position-1):
+		current = current.nextNode
+	current.nextNode = current.nextNode.nextNode
+	return position
+
+# Elimina un elemento por posición de la lista
+# Poscondición:​ Se debe desvincular el ​ Node​ a eliminar.
+# Salida​ : Devuelve la posición donde se encuentra el elemento a eliminar. Devuelve ​ None​ si el elemento a eliminar no se encuentra
+def deletePosition(L,position):
+	current = L.head
+	node = accessNode(L, position)
+	if node == None: return
+	if position == 0: 
+		L.head = L.head.nextNode
+		return position
+
+	for i in range(0, position-1):
+		current = current.nextNode
+	current.nextNode = current.nextNode.nextNode
+	return position
+
+# Calcula el número de elementos de la lista
+# Salida​ : Devuelve el número de elementos.
+def length(L):
+	current = L.head
+	currentPos = 0
+
+	while current != None:
+		current = current.nextNode
+		currentPos += 1
+	return currentPos
+
+# Permite acceder a un elemento de la lista en una posición determinada.
+# Salida​: Devuelve el valor de un elemento en una position de la lista, devuelve​ None​ si no existe elemento​ para dicha posición.
+def access(L,position):
+	if position >= 0:
+		current = L.head
+
+		for i in range(0, position):
+			if current == None: return
+			current = current.nextNode
+		return current.value
+	return
+
+# Permite acceder a un elemento de la lista en una posición determinada.
+# Salida​: Devuelve el valor de un elemento en una position de la lista, devuelve​ None​ si no existe elemento​ para dicha posición.
+def accessNode(L,position):
+	if position >= 0:
+		current = L.head
+
+		for i in range(0, position):
+			if current == None: return
+			current = current.nextNode
+		return current
+	return
+
+# Permite cambiar el valor de un elemento de la lista en una posición determinada
+# Salida​ : Devuelve ​ None si no existe elemento ​ para dicha posición. Caso contrario devuelve la posición donde pudo hacer el update.
+def update(L,element,position):
+	if position >= 0:
+		current = L.head
+
+		for i in range(0, position):
+			current = current.nextNode
+			if current == None: return
+		current.value = element
+		return position
+	return
+
+def revert(L):
+	newL = LinkedList()
+	len = length(L)
+	current = L.head
+
+	for i in range(len):
+		len -= 1
+		add(newL, current.value)
+		current = current.nextNode
+	return newL
+
+def print_list(L):
+	current = L.head
+	currentPos = 0
+
+	while current != None:
+		if currentPos != 0: print(end=" -> ")
+		print(current.value, end="")
+		current = current.nextNode
+		currentPos += 1
+	print()
+	return currentPos
+
+from random import randint
+
+def random_list(length, rnd_from, rnd_to):
+	L = LinkedList()
+	for i in range(length):
+		add(L, randint(rnd_from, rnd_to))
+	return L
+
+def moveNode(L, position_orig, position_dest):
+	if position_orig == position_dest: return
+	elif position_orig == 0:
+		# La head es la posición de origen
+		originalNode = L.head
+
+		L.head = L.head.nextNode
+		previousDest = previousNode(L, position_dest)
+
+		originalNode.nextNode = previousDest.nextNode
+		previousDest.nextNode = originalNode
+	elif position_dest == 0:
+		# La head es la posición de destino
+		previousOrig = previousNode(L, position_orig)
+		originalNode = previousOrig.nextNode
+
+		if previousOrig.nextNode != None:
+			previousOrig.nextNode = previousOrig.nextNode.nextNode
+		else: previousOrig.nextNode = None
+
+		originalNode.nextNode = L.head
+		L.head = originalNode
+	else:
+		previousOrig = previousNode(L, position_orig)
+		originalNode = previousOrig.nextNode
+
+		if previousOrig.nextNode != None:
+			previousOrig.nextNode = previousOrig.nextNode.nextNode
+		else: previousOrig.nextNode = None
+
+		previousDest = previousNode(L, position_dest)
+		originalNode.nextNode = previousDest.nextNode
+		previousDest.nextNode = originalNode
+
+def previousNode(L, position):
+	count = 0
+	current = L.head
+	while count < position-1:
+			current = current.nextNode
+			count += 1
+	return current
+
+def swapNodes(list, nodeAPos, nodeBPos):
+	nodeB = accessNode(list, nodeBPos)
+
+	moveNode(list, nodeAPos, nodeBPos)
+	nodeBnewPos = searchNode(list, nodeB)
+	moveNode(list, nodeBnewPos, nodeAPos)
+
